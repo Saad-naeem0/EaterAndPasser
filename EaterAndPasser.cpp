0bdd2ab7125903e34924ced0701745909043e286
Saad-naeem0 <100247988+Saad-naeem0@users.noreply.github.com>
2022-12-26 02:42:36 +0500

    firstCommit

--- EaterAndPasser.cpp
+++ EaterAndPasser.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 main()
 {
-	printf("lets Go!");
-	return 0;
+	char board[9][9], row, col;
+	do{
+	
+	
+	for(int i=0; i<3; i++)
+	{
+		printf("%d",i);
+		for(int j=0; j<3; j++)
+		{
+			printf("| %c |",board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Enter next move: ");
+	scanf("%d %d", &row, &col);
+	}
+	while(board!='/0');	
+	printf("board is full");
 }
